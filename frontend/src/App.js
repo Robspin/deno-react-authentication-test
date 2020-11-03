77bec103ec92b4a45a5211929b116e7c7fafe661
Robspin <robin_steeman@hotmail.com>
2020-11-03 15:41:42 +0100

    added redux and created loginpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { Container } from 'react-bootstrap';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -13,6 +14,7 @@ const App = () => {
          <main className='py-3'>
             <Container>
                <Route path='/' component={HomePage} exact />
+               <Route path='/login' component={LoginPage} />
             </Container>
          </main>
          <Footer />
